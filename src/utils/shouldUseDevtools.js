4dad28539c394a4907c732d5d4deb067d0a77b7b
James Gillmore <james@faceyspacey.com>
2024-05-08 03:14:28 -0700

    0.0.69

--- src/utils/shouldUseDevtools.js
+++ src/utils/shouldUseDevtools.js
@@ -1,4 +1,4 @@
 export default options => {
-  if (process.env.NODE_ENV === 'production' && options.enableDevtoolsInProduction) return false
+  if (process.env.NODE_ENV === 'production') return false
   return process.env.NODE_ENV !== 'test' && process.env.WEB && typeof window !== undefined && window.__REDUX_DEVTOOLS_EXTENSION__
 }
