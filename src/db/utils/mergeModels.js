01b813538f0d77f5672fa54559cc39722c62173f
James Gillmore <james@faceyspacey.com>
2024-05-22 01:41:50 -0700

    0.0.98

--- src/db/utils/mergeModels.js
+++ src/db/utils/mergeModels.js
@@ -1,9 +1,9 @@
-export default ({ shared = {}, server = {} } = {}) => {
-  const keys = Object.keys({ ...shared, ...server })
+export default ({ shared = {}, client = {} } = {}) => {
+  const keys = Object.keys({ ...shared, ...client })
 
   return keys.reduce((acc, k) => {
     const s = g(shared[k] || {})
-    const m = g(server[k] || {})
+    const m = g(client[k] || {})
 
     acc[k] = Object.assign(acc, s, m)
     return acc
