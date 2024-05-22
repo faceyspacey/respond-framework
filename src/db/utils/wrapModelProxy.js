afb570a6ce85c938ecc90c6f03a787869e91c9e5
James Gillmore <james@faceyspacey.com>
2024-05-22 02:54:51 -0700

    0.0.100

--- src/db/utils/wrapModelProxy.js
+++ src/db/utils/wrapModelProxy.js
@@ -1,30 +1,34 @@
 export default (name, doc, models, state) => {
   if (!doc) return
   
-  const model = models[name]
+  const model = models[name] // model is descriptors
 
   if (!model) return doc
 
   const proxy = new Proxy(doc, {
-    get: (target, k, receiver) => {
-      const descriptor = model[k]
-      const getter = descriptor?.get
-
-      if (getter) {
-        return getter.call(proxy)
-      }
-
-      const method = descriptor?.value
-
-      if (typeof method === 'function') {
-        return (...args) => method.apply(proxy, args)
-      }
-
-      if (k === '_state') return state // escape hatch to access module state within class methods
-
-      return Reflect.get(target, k, receiver)
-    },
+    get: (target, k) => callModelMethod(model, proxy, target, k),
   })
 
   return proxy
+}
+
+
+export const callModelMethod = (descriptors, proxy, target = {}, k) => {
+  const v = target[k]
+  if (v !== undefined) return v
+
+  const descriptor = descriptors[k]
+  const getter = descriptor?.get
+
+  if (getter) {
+    return getter.call(proxy)
+  }
+
+  const method = descriptor?.value
+
+  if (typeof method === 'function') {
+    return (...args) => method.apply(proxy, args)
+  }
+
+  if (k === '_state') return state // escape hatch to access module state within class methods
 }
