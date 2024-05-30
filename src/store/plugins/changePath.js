d861ed2724d14dee879a56e3cd7a09a2ee24706e
James Gillmore <james@faceyspacey.com>
2024-05-29 22:27:33 -0700

    0.0.140

--- src/store/plugins/changePath.js
+++ src/store/plugins/changePath.js
@@ -2,13 +2,13 @@ export default async (store, e) => {
   if (e.meta.parallel && !e.meta.changePath) return
 
   if (e.event.path) {
-    store.changePath(e)
+    store.history.changePath(e)
     return
   }
 
-  const curr = e.event.findDrainEvent?.call(e.event, store, e)
+  const curr = e.event.changePath?.call(e.event, store, e)
 
   if (curr) {
-    store.changePath(curr, true)
+    store.history.changePath(curr, true)
   }
 }
