71f1327b1e76f5458fc9978984b9743ee2d4518e
James Gillmore <james@faceyspacey.com>
2025-01-08 22:11:40 -0800

    asf

--- src/proxy/snapshot.js
+++ src/proxy/snapshot.js
@@ -7,10 +7,6 @@ export default function(proxy = this.state, vns = this.versionNotifiers, cache =
   return vn ? createSnapshot(vn, vns, cache) : cloneDeep(proxy) // because snapshots before proxication otherwise return the entire original object, and need to be separate references -- important for triggerPlugin's creation of prevState, which must not share references with the current state, or prevState will be the same as current state
 }
 
-function snapshot(proxy, vns, cache) {
-  const vn = vns.get(proxy)
-  return vn ? createSnapshot(vn, vns, cache) : proxy // proxy : primitive value
-}
 
 function createSnapshot({ orig: o, version }, vns, cache) {
   const { version: v, snap: s } = cache.get(o) ?? {}
@@ -19,7 +15,13 @@ function createSnapshot({ orig: o, version }, vns, cache) {
   const snap = isArray(o) ? [] : create(getProto(o)) // computed methods/selectors on prototype
   cache.set(o, { snap, version })
 
-  keys(o).forEach(k => snap[k] = snapshot(o[k], vns, cache))
+  keys(o).forEach(k => snap[k] = recurse(o[k], vns, cache))
 
   return snap
+}
+
+
+function recurse(proxy, vns, cache) {
+  const vn = vns.get(proxy)
+  return vn ? createSnapshot(vn, vns, cache) : proxy // proxy : primitive value
 }
