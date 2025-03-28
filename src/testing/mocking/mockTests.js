3bf1e9242fd498659594a78dd6bf1b5e299b9ec4
James Gillmore <james@faceyspacey.com>
2025-03-28 01:46:28 -0700

    asdf

--- src/testing/mocking/mockTests.js
+++ src/testing/mocking/mockTests.js
@@ -10,3 +10,35 @@ export default () => {
   mockDirectory('icons')
   mockDirectory('widgets', 'ignoredWidgets')
 }
+
+
+
+
+// const originalDate = Date
+
+// function mockTimezone(timezoneOffset) {
+//   globalThis.Date = class extends originalDate {
+//     constructor(...args) {
+//       if (args.length === 0) {
+//         const utcDate = new originalDate()
+//         const utcTime = utcDate.getTime()
+//         const localTime = utcTime + (timezoneOffset * 60 * 1000)
+
+//         super(localTime)
+//       }
+//       else {
+//         super(...args)
+//       }
+//     }
+//   };
+// }
+
+
+// // mockTimezone(-7 * 60)
+
+
+// const today = new Date('2023-07-04T12:00:00.000-07:00')
+
+// jest
+//   .useFakeTimers()
+//   .setSystemTime(today)
