0a9061bbd1934bbe128deeabff6a03b6a62517a6
James Gillmore <james@faceyspacey.com>
2025-03-31 23:04:53 -0700

    ctx.sync = false

--- src/createModule/plugins/edit/index.js
+++ src/createModule/plugins/edit/index.js
@@ -11,10 +11,12 @@ import end from '../end.js'
 // note: even though this runs in an await dispatch, it's still sync, since it still happens syncronously before any awaited promises complete
 export default function edit(state, e) {
   if (!e.event.sync) return
-  state.respond.ctx.sync = true // prevent standard queued microtask listeners from being notified
+
+  const { ctx } = state.respond
+  ctx.sync = true // prevent standard queued microtask listeners from being notified
 
   const res = dispatchPlugins(plugins, state, e)
-  return res instanceof Promise ? res.then(_ => false) : false
+  return res instanceof Promise ? res.then(_ => ctx.sync = false) : ctx.sync = false
 }
 
 edit.sync = true // mark event as sync, so createPlugins can shift em to front of dispatch pipeline before potentially async events
